41551c1adc179163cd75b899f6175f3df8daeea8
shareef213 <shareef.l@psu.ac.th>
2018-09-09 14:43:07 +0700

    save authen fisrt

---
+++ src/pages/home/home.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { SignupPage } from '../signup/signup';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public email: string = "";
+  public password: string = "";
+  public error: string = "";
+  public notic: string = "";
+  public data: Observable<any>;
+
+  constructor(public navCtrl: NavController,
+    public http: HttpClient) {
+
+  }
+
+  login() {
+    let frmData = new FormData();
+    frmData.append('email', this.email);
+    frmData.append('password', this.password);
+    this.data = this.http.post('http://localhost/ionic_api/api_signin.php', frmData);
+    this.data.subscribe(resp => {
+      this.notic = resp.message;
+    }, error => this.error = error);
+  }
+
+  gotoSignup() {
+    this.navCtrl.push(SignupPage);
+  }
+
+}
